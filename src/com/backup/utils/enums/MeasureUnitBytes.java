97af9ae3efefe1c43a3e2e4bbca1ff7d75a06c88
Allan Jacques <allanjnofs@gmail.com>
2015-02-03 09:02:52 -0300

    corrigindo erros com git

--- src/com/backup/utils/enums/MeasureUnitBytes.java
+++ src/com/backup/utils/enums/MeasureUnitBytes.java
@@ -5,17 +5,10 @@ package com.backup.utils.enums;
  */
 public enum MeasureUnitBytes {
 
-<<<<<<< HEAD
-    GIGA(1024 ^ 3, "Gigabytes"),
-    MEGA(1024 ^ 2, "Megabytes"),
-    KILO(1024 ^ 1, "Kilobytes"),
-    TERA(1024 ^ 4, "Terabytes");
-=======
-    GIGA(((long)Math.pow(1024, 3)), "Gigabytes"),
-    MEGA(((long)Math.pow(1024, 2)), "Megabytes"),
-    KILO(((long)Math.pow(1024, 1)), "Kilobytes"),
-    TERA(((long)Math.pow(1024, 4)), "Terabytes");
->>>>>>> 063be23b456479b07e4bca8adbbc8fe27f44dcc5
+    GIGA(((long) Math.pow(1024, 3)), "Gigabytes"),
+    MEGA(((long) Math.pow(1024, 2)), "Megabytes"),
+    KILO(((long) Math.pow(1024, 1)), "Kilobytes"),
+    TERA(((long) Math.pow(1024, 4)), "Terabytes");
 
     private MeasureUnitBytes(final long bytes, final String description) {
         this.bytes = bytes;
